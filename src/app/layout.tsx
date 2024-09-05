49c1f72b867e7e4b36a6ddae675e14b49e01735a
Antonio Gallardo Girela <gallardogirela@gmail.com>
2024-09-05 12:21:40 +0200

    feat: add Convex and create ConvexClientProvider component
    - Install Convex dependency using bunx add convex.
    - Implement ConvexClientProvider component to set up Convex client.
    - Initialize ConvexReactClient with the environment
      variable NEXT_PUBLIC_CONVEX_URL.
    - Wrap children components with ConvexProvider to provide
      Convex context throughout the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next"
+
 import localFont from "next/font/local"
+
 import "./globals.css"
+
+import { ConvexClientProvider } from "@/components/convex-client-provider"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   )
