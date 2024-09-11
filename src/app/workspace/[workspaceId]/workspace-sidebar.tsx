c217f1cbbb70b8704ee530dccd701c258288a22d
Antonio Gallardo Girela <gallardogirela@gmail.com>
2024-09-11 11:52:18 +0200

    feat: add channel and member management, workspace sidebar updates
    - Install "react-use" package for additional hooks support.
    - Add channels to workspaces and create a
      default "general" channel on workspace creation.
    - Implement the retrieval and display of channels
      and members in the workspace sidebar.
    - Update schema with a new channels table and
      corresponding indexes.
    - Enhance the workspace sidebar with sections for
      threads, drafts, channels, and direct messages.
    - Improve `WorkspaceSidebar` to include dynamic
      listing of channels and members.
    - Modify user button avatar to be rounded
      and improve styling.

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,10 +1,21 @@
-import { AlertTriangle, Loader } from "lucide-react"
+import {
+  AlertTriangle,
+  HashIcon,
+  Loader,
+  MessageSquareText,
+  SendHorizonal,
+} from "lucide-react"
 
 import { WorkspaceHeader } from "./workspace-header"
+import { SidebarItem } from "./sidebar-item"
+import { UserItem } from "./user-item"
+import { WorkspaceSection } from "./workspace-section"
 
+import { useWorkspaceId } from "@/hooks/use-workspace-id"
 import { useCurrentMember } from "@/features/members/api/use-current-member"
 import { useGetWorkspace } from "@/features/worskpaces/api/use-get-workspace"
-import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { useGetChannels } from "@/features/channels/api/use-get-channels"
+import { useGetMembers } from "@/features/members/api/use-get-members"
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId()
@@ -14,6 +25,14 @@ export const WorkspaceSidebar = () => {
   })
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
     id: workspaceId,
+  })
+
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  })
+
+  const { data: members, isLoading: membersLoading } = useGetMembers({
+    workspaceId,
   })
 
   if (workspaceLoading || memberLoading) {
@@ -39,6 +58,34 @@ export const WorkspaceSidebar = () => {
         workspace={workspace}
         isAdmin={member.role === "admin"}
       />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+        <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
+      </div>
+      <WorkspaceSection label="Channels" hint="New channel" onNew={() => {}}>
+        {channels?.map((item) => (
+          <SidebarItem
+            key={item._id}
+            icon={HashIcon}
+            label={item.name}
+            id={item._id}
+          />
+        ))}
+      </WorkspaceSection>
+      <WorkspaceSection
+        label="Direct Messages"
+        hint="New direct message"
+        onNew={() => {}}
+      >
+        {members?.map((item) => (
+          <UserItem
+            key={item._id}
+            id={item._id}
+            label={item.user.name}
+            image={item.user.image}
+          />
+        ))}
+      </WorkspaceSection>
     </div>
   )
 }
