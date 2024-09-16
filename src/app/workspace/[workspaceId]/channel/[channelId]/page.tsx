e65aff82b7ef6d6f2ed459a85c8718e543ec76d5
Antonio Gallardo Girela <gallardogirela@gmail.com>
2024-09-16 18:12:08 +0200

    feat: add date-fns, improve message list with pagination and avatar UI updates
    - Add `date-fns` for date formatting and manipulation
    - Enhance message list with custom scrollbar styling and pagination support
    - Implement `loadMore` functionality in `MessageList` with loading status checks
    - Update avatar design from circular to rounded square for consistent UI

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -8,19 +8,18 @@ import { useChannelId } from "@/hooks/use-channel-id"
 
 import { Header } from "./header"
 import { ChatInput } from "./chat-input"
+import { MessageList } from "@/components/message-list"
 
 const ChannelIdPage = () => {
   const channelId = useChannelId()
 
-  const { results } = useGetMessages({ channelId })
+  const { results, status, loadMore } = useGetMessages({ channelId })
 
   const { data: channel, isLoading: channelLoading } = useGetChannel({
     id: channelId,
   })
 
-  console.log({ results })
-
-  if (channelLoading) {
+  if (channelLoading || status === "LoadingFirstPage") {
     return (
       <div className="h-full flex-1 flex items-center justify-center">
         <Loader className="animate-spin size-5 text-muted-foreground" />
@@ -40,7 +39,14 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1">{JSON.stringify(results)}</div>
+      <MessageList
+        channelName={channel.name}
+        channelCreationTime={channel._creationTime}
+        data={results}
+        loadMore={loadMore}
+        isLoadingMore={status === "LoadingMore"}
+        canLoadMore={status === "CanLoadMore"}
+      />
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   )
