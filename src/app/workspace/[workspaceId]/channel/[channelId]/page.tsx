48914e65546c0ed0166cf2547a65e91e49103c52
Antonio Gallardo Girela <gallardogirela@gmail.com>
2024-09-16 14:08:04 +0200

    feat(messages): add query to fetch messages with threads and reactions
    - Add `get` query to fetch messages by channel, conversation, or parent message ID
    - Populate reactions and threads for messages
    - Include reactions with member counts and unique reactions
    - Add thread population logic to include the last message in a thread
    - Update message schema to include `updatedAt` as an optional field
    - Introduce index for `parentMessageId` to support thread querying

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -2,6 +2,7 @@
 
 import { Loader, TriangleAlert } from "lucide-react"
 
+import { useGetMessages } from "@/features/messages/api/use-get-messages"
 import { useGetChannel } from "@/features/channels/api/use-get-channel"
 import { useChannelId } from "@/hooks/use-channel-id"
 
@@ -11,9 +12,13 @@ import { ChatInput } from "./chat-input"
 const ChannelIdPage = () => {
   const channelId = useChannelId()
 
+  const { results } = useGetMessages({ channelId })
+
   const { data: channel, isLoading: channelLoading } = useGetChannel({
     id: channelId,
   })
+
+  console.log({ results })
 
   if (channelLoading) {
     return (
@@ -35,7 +40,7 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1" />
+      <div className="flex-1">{JSON.stringify(results)}</div>
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   )
