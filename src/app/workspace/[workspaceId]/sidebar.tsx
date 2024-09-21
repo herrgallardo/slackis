9769d3b86dc4186211355ef10d847a739066b2c1
Antonio Gallardo Girela <gallardogirela@gmail.com>
2024-09-21 11:26:48 +0200

    refactor(sidebar): add workspace links and adjust layout
    - Update Sidebar buttons to use `Link` components for better navigation
    - Remove redundant sidebar items from the WorkspaceSidebar component

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,26 +1,36 @@
+import Link from "next/link"
 import { usePathname } from "next/navigation"
-
 import { Bell, Home, MessagesSquare, MoreHorizontal } from "lucide-react"
 
-import { SidebarButton } from "./siderbar-button"
-
 import { UserButton } from "@/features/auth/components/user-button"
+import { useWorkspaceId } from "@/hooks/use-workspace-id"
+
+import { SidebarButton } from "./siderbar-button"
 import { WorkspaceSwitcher } from "./workspace-switcher"
 
 export const Sidebar = () => {
   const pathname = usePathname()
+  const workspaceId = useWorkspaceId()
 
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-[4px]">
       <WorkspaceSwitcher />
-      <SidebarButton
-        icon={Home}
-        label="Home"
-        isActive={pathname.includes("/workspace")}
-      />
-      <SidebarButton icon={MessagesSquare} label="DMs" />
-      <SidebarButton icon={Bell} label="Activity" />
-      <SidebarButton icon={MoreHorizontal} label="More" />
+      <Link href={`/workspace/${workspaceId}`}>
+        <SidebarButton
+          icon={Home}
+          label="Home"
+          isActive={pathname.includes("/workspace/")}
+        />
+      </Link>
+      <Link href={`/workspace/${workspaceId}`}>
+        <SidebarButton icon={MessagesSquare} label="DMs" />
+      </Link>
+      <Link href={`/workspace/${workspaceId}`}>
+        <SidebarButton icon={Bell} label="Activity" />
+      </Link>
+      <Link href={`/workspace/${workspaceId}`}>
+        <SidebarButton icon={MoreHorizontal} label="More" />
+      </Link>
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
